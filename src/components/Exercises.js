738c9ee411488ba0040b818f15ae196ac87b35c7
wagnerAJS221 <wagneralex221@gmai.com>
2023-04-18 17:52:51 +0100

    exercises names rendering. now to add the image/gifs for the exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Pagination } from '@mui/material/Pagination'
+import { Box, Stack, Typography } from '@mui/material/'
 
-const Exercises = () => {
-  return <div></div>
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log('Exercises', exercises)
+  return (
+    <Box id="exercises" sx={{ mt: { lg: '110px' } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="40px">
+        Showing results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: '110px', xs: '50px' } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  )
 }
 
 export default Exercises
